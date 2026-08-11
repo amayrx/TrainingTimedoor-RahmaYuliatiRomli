a984b70442440103679077d30eb318f45e6aaec5
Rahma Yuliati Romli <158243313+amayrx@users.noreply.github.com>
2026-08-11 09:45:49 +0700

    Refactor script.js to clean up and add display pattern
    Removed commented-out code and added a new pattern display.

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
-console.log('    *    ');
-console.log('   ***   ');
-console.log('  *****  ');
-console.log(' ******* ');
-console.log('   |||   ');
-
-
-// // This is variable for first name
-// var firstName = "Rahma Yuliati";
-
-// // This is variable for last name
-// var lastName = "Romli";
-
-// // This is my favorite number
-// var favoriteNumber = 10;
-
-// // Display the value of firstName to the console
-// console.log(firstName);
-
-// // Display the value of lastName to the console
-// console.log(lastName);
-
-// // Display the value of favoriteNumber to the console
-// console.log(favoriteNumber);
-
-// var umur = 8
-
-// if (umur >= 17) {
-//     console.log("Eligible");
-// }else if (umur < 17) {
-//     console.log("Not Eligible");
-// }
-
-// var nilai = 50
-
-// if (nilai > 90) {
-//     console.log("Grade A");
-// }else if (nilai > 75) {
-//     console.log("Grade B");
-// }else if (nilai > 60) {
-//     console.log("Grade C");
-// }else if (nilai < 60) {
-//     console.log("Grade D");
-// }
+console.log('     *     ');
+console.log('    ***    ');
+console.log('   *****   ');
+console.log('  *******  ');
+console.log('    |||    ');
