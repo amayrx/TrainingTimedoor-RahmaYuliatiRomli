81b28be27012e572f73bfb03bd78e54dc936b5df
Rahma Yuliati Romli <158243313+amayrx@users.noreply.github.com>
2026-08-12 20:36:19 +0700

    Create lesson5.js

---
+++ lesson5.js
@@ -0,0 +1,9 @@
+function intro() {
+    document.getElementById("result").innerHTML =
+        "Hello! My name is Steve. Nice to meet you!";
+}
+
+function funFact() {
+    document.getElementById("result").innerHTML =
+        "Fun fact: I love playing games and learning to code!";
+}
