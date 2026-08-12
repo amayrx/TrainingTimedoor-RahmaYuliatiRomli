8f90006307ea5634c83d68fd652e829ab1b64097
Rahma Yuliati Romli <158243313+amayrx@users.noreply.github.com>
2026-08-12 13:00:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,28 +6,3 @@ console.log('   *****   ');
 console.log('  *******  ');
 console.log('    |||    ');
 
-//Lesson 2
-// Code Practice 2 - Task 1
-
-let title = "Captain Marvel";
-let rating = 6.8;
-let year = 2019;
-let forAllAges = false;
-
-console.log("Title:", title);
-console.log("Rating:", rating);
-console.log("Year of Issue:", year);
-console.log("For all ages:", forAllAges);
-
-
-// Code Practice 2 - Task 2
-
-let bookTitle = "The Quantum Universe";
-let author = "Brian Cox and Jeff Forshaw";
-let yearPublished = 2011;
-let isScience = true;
-
-console.log("Book Title:", bookTitle);
-console.log("Author:", author);
-console.log("Year Published:", yearPublished);
-console.log("Is Science:", isScience);
